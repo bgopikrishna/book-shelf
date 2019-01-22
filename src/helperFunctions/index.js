d43846d6d282133e2d56e5cf3831db8ab410168b
bgopikrishna <gopiwayz@gmail.com>
2019-01-22 15:38:55 +0530

    Added toast with emoji

--- src/helperFunctions/index.js
+++ src/helperFunctions/index.js
@@ -1,9 +1,9 @@
 import * as M from "materialize-css";
 
 const shelfParser = {
-  toR: "To Read",
-  cR: "Currently Reading",
-  r: "read"
+  toR: " To Read",
+  cR: " Currently Reading",
+  r: " Read"
 };
 
 //Parsing the Fetched Data from the api
@@ -52,12 +52,17 @@ export const bookShelfManager = (books, id, shelf, toast = false) => {
     .map(book => {
       if (book.id === id) {
         book.shelf = shelf;
+        // iPopping toast if it's true
         if (toast) {
+          const toastMsg = `"${book.title}" moved to shelf &nbsp;&nbsp;"${
+            shelfParser[shelf]
+          }"&nbsp;&nbsp;📕 `;
+
+          //Showing toast
           M.toast({
-            html: `"${book.title}" moved to shelf ${shelfParser[shelf]}`
+            html: toastMsg
           });
         }
-        // if callback exists the callback function works
       }
       return book;
     })
